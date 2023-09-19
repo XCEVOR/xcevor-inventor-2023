219dae6d2fd2f40cfc363ffd411ca65b699396e9
XCEVOR <XCEVOR@GMAIL.COM>
2023-09-19 14:57:14 +0900

    feat: Add dynamic progress bar

---
+++ src/components/about-comps/ProgressBar.tsx
@@ -0,0 +1,56 @@
+// components/ProgressBar.tsx
+"use client"
+
+import React, { useState, useEffect } from "react";
+
+interface ProgressBarProps {
+  bgcolor: string;
+  completed: number;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ bgcolor, completed }) => {
+  const [fillerWidth, setFillerWidth] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFillerWidth(Math.floor(Math.random() * 100) + 1);
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const containerStyles: React.CSSProperties = {
+    height: 20,
+    width: '50%',
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
+    margin: 50,
+  };
+
+  const fillerStyles: React.CSSProperties = {
+    height: '100%',
+    width: `${fillerWidth}%`,
+    backgroundColor: bgcolor,
+    transition: 'width 1s ease-in-out',
+    borderRadius: 'inherit',
+    textAlign: 'right',
+  };
+
+  const labelStyles: React.CSSProperties = {
+    padding: 5,
+    color: 'white',
+    fontWeight: 'bold',
+  };
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${completed}%`}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- src/components/about-comps/Skills.tsx
+++ src/components/about-comps/Skills.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import ProgressBar from '@/components/about-comps/ProgressBar';
 
 const Skills = () => {
+  const testData = [
+    { bgcolor: "#6a1b9a", completed: 60 },
+    { bgcolor: "#00695c", completed: 30 },
+    { bgcolor: "#ef6c00", completed: 53 },
+  ];
+
+
   return (
-    <div>Skills</div>
+    <div>
+      Skills
+      <div>
+        <h1>Skills</h1>
+        {testData.map((item, idx) => (
+        <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+      ))}
+      </div>
+    </div>
   )
 }
 
