fd04b897d2f2cafcdfe13a0de2408de560678be3
XCEVOR <XCEVOR@GMAIL.COM>
2023-09-19 14:48:17 +0900

    feat: Modify flex-row for large screen

--- src/components/about-comps/Me.tsx
+++ src/components/about-comps/Me.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const Me = () => {
   return (
-    <div className='flex flex-col h-[calc(100vh-3rem)] md:h-[calc(100vh-6rem)]'>
+    <div className='flex flex-col h-[calc(100vh-3rem)] md:h-[calc(100vh-6rem)] lg:flex-row'>
 
-      <div className='h-1/2 flex items-center justify-center flex-col gap-8 text-[#ffbf00] font-bold'>
+      <div className='flex flex-col flex-1 items-center justify-center gap-8 text-[#ffbf00] font-bold'>
         <h1 className='text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl'>
           me
         </h1>
       </div>
-      <div className='w-full h-1/2 relative'>
+      <div className='flex-1 w-full relative'>
         <Image src="/img/me.jpg" alt='' fill className='object-cover' />
       </div>
     </div>
