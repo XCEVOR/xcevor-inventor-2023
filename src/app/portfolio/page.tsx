d6a5007b673aeeb5ae95187c6d0df01d4af0b5ab
XCEVOR <XCEVOR@GMAIL.COM>
2023-09-19 16:00:26 +0900

    feat: Cteate single portfolio page and modify link

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -24,7 +24,7 @@ const Portfolio = () => {
 
     <div className="flex flex-wrap text-[#ffbf00]">
       {pizzas.map((item) => (
-        <Link className="w-full h-[60vh] border-r-2 border-b-2 border-[#ffbf00] sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group odd:bg-fuchsia-50" href={`/product/${item.id}`} key={item.id}>
+        <Link className="w-full h-[60vh] border-r-2 border-b-2 border-[#ffbf00] sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group odd:bg-fuchsia-50" href={`/portfolio/${item.id}`} key={item.id}>
           {/* IMAGE CONTAINER */}
           {/* {item.img && (
             <div className="relative h-[80%]">
