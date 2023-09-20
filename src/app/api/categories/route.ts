fa895b8e403fd191da1c2ceb1d439979038c9efb
XCEVOR <XCEVOR@GMAIL.COM>
2023-09-20 16:52:49 +0900

    feat: Create PrismaClient for fetching cat data

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,5 +1,23 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server"
 
-export const GET = () => {
+const prisma = new PrismaClient();
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(
+      JSON.stringify( JSON.stringify(categories) ), { status: 500 }
+    );
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong! "}), { status: 500 }
+    );
+  }
   return new NextResponse("Hello~~", { status: 200 });
+}
+
+export const POST = () => {
+  return new NextResponse("POST Hello~~", { status: 200 });
 }
