f2639e40c351e7f6d689e516c453aa275c10019f
XCEVOR <XCEVOR@GMAIL.COM>
2023-09-23 00:51:55 +0900

    feat: Apply page-wrapper on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image'
 
 import BackgroundImage from '@/components/BackgroundImage';
+import { PageWrapper } from '@/components/framer-motion/page-wrapper';
 
 
 export default function Home() {
   return (
     <>
-    <BackgroundImage />
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hollo!!</h1>
-    </main>
+      <BackgroundImage />
+
+      <PageWrapper>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <h1>Hollo!!</h1>
+        </main>
+      </PageWrapper>
     </>
   )
 }
