96b1b8e7353399b3179d244216cd6aa6ae7d0e44
XCEVOR <XCEVOR@GMAIL.COM>
2023-09-27 14:01:47 +0900

    feat: Create and Add dummy data in docker

--- docker/04-_Productdescription__202309221520.sql
+++ docker/04-_Productdescription__202309221520.sql
@@ -1,5 +1,41 @@
+-- Productdescription 테이블에 더미 데이터 추가
 INSERT INTO public."Productdescription" (id,"desc","titleSlug") VALUES
-	 ('clmtc9bjn0000ua8oab9msj9s','aaaaa','app1'),
-	 ('clmtc9bjn0001ua8oz4c5apg3','bbbbb','app1'),
-	 ('clmtd5ct30002ua8ocqm6huyz','ccccc','app1'),
-	 ('clmtdhi070003ua8oq3lktm74','ddddd','app2');
+   ('clmtc9bjn0000ua8oab9msj9s','Boost your productivity with our innovative Productivity App.','Productivity App'),
+   ('clmtc9bjn0001ua8oz4c5apg3','Increase efficiency and achieve your goals with our Productivity App.','Productivity App'),
+   ('clmtc9bjn0002ua8oz4c5apg3','Stay organized and get things done with our Productivity App.','Productivity App'),
+   ('clmtc9bjn0003ua8oz4c5apg3','Streamline your tasks and manage your time effectively with our Productivity App.','Productivity App'),
+   
+   ('clmt1a9bjn0000ua8oab9msj9s','Stay active and healthy with our Health & Fitness Tracker app.','Health & Fitness Tracker'),
+   ('clmt1a9bjn0001ua8oz4c5apg3','Track your fitness progress and reach your health goals with our Health & Fitness Tracker app.','Health & Fitness Tracker'),
+   ('clmt1a9bjn0002ua8oz4c5apg3','Monitor your workouts and stay in shape with our Health & Fitness Tracker app.','Health & Fitness Tracker'),
+   ('clmt1a9bjn0003ua8oz4c5apg3','Stay motivated and achieve a healthier lifestyle with our Health & Fitness Tracker app.','Health & Fitness Tracker'),
+   
+   ('clmt2a9bjn0000ua8oab9msj9s','Manage your finances easily with our Expense Manager app.','Expense Manager'),
+   ('clmt2a9bjn0001ua8oz4c5apg3','Keep track of your expenses and budget with our Expense Manager app.','Expense Manager'),
+   ('clmt2a9bjn0002ua8oz4c5apg3','Control your spending and save money with our Expense Manager app.','Expense Manager'),
+   ('clmt2a9bjn0003ua8oz4c5apg3','Simplify your financial management with our Expense Manager app.','Expense Manager'),
+   
+   ('clmt3a9bjn0000ua8oab9msj9s','Discover new recipes and flavors with our Recipe Finder app.','Recipe Finder'),
+   ('clmt3a9bjn0001ua8oz4c5apg3','Cook delicious meals and try new cuisines with our Recipe Finder app.','Recipe Finder'),
+   ('clmt3a9bjn0002ua8oz4c5apg3','Find the perfect recipe for any occasion with our Recipe Finder app.','Recipe Finder'),
+   ('clmt3a9bjn0003ua8oz4c5apg3','Enjoy cooking and explore a world of culinary delights with our Recipe Finder app.','Recipe Finder'),
+
+   ('clmt4a9bjn0000ua8oab9msj9s','Explore new destinations and plan your next adventure with our Travel Planner app.','Travel Planner'),
+   ('clmt4a9bjn0001ua8oz4c5apg3','Plan your trips efficiently and create unforgettable memories with our Travel Planner app.','Travel Planner'),
+   ('clmt4a9bjn0002ua8oz4c5apg3','Discover hidden gems and experience the world like never before with our Travel Planner app.','Travel Planner'),
+   ('clmt4a9bjn0003ua8oz4c5apg3','Turn your travel dreams into reality with our Travel Planner app.','Travel Planner'),
+
+   ('clmt5a9bjn0000ua8oab9msj9s','Stay informed about the weather with our accurate Weather Forecast app.','Weather Forecast'),
+   ('clmt5a9bjn0001ua8oz4c5apg3','Get real-time weather updates and forecasts with our Weather Forecast app.','Weather Forecast'),
+   ('clmt5a9bjn0002ua8oz4c5apg3','Plan your outdoor activities and be weather-ready with our Weather Forecast app.','Weather Forecast'),
+   ('clmt5a9bjn0003ua8oz4c5apg3','Never be caught off guard by the weather again with our Weather Forecast app.','Weather Forecast'),
+
+   ('clmt6a9bjn0000ua8oab9msj9s','Take control of your budget and expenses with our efficient Budget Tracker app.','Budget Tracker'),
+   ('clmt6a9bjn0001ua8oz4c5apg3','Set financial goals and achieve them with our Budget Tracker app.','Budget Tracker'),
+   ('clmt6a9bjn0002ua8oz4c5apg3','Track every expense and make informed financial decisions with our Budget Tracker app.','Budget Tracker'),
+   ('clmt6a9bjn0003ua8oz4c5apg3','Experience financial freedom and peace of mind with our Budget Tracker app.','Budget Tracker'),
+
+   ('clmt7a9bjn0000ua8oab9msj9s','Find inner peace and relaxation with our Meditation Guide app.','Meditation Guide'),
+   ('clmt7a9bjn0001ua8oz4c5apg3','Practice mindfulness and reduce stress with our Meditation Guide app.','Meditation Guide'),
+   ('clmt7a9bjn0002ua8oz4c5apg3','Discover inner calm and improve your mental well-being with our Meditation Guide app.','Meditation Guide'),
+   ('clmt7a9bjn0003ua8oz4c5apg3','Achieve mental clarity and a sense of tranquility with our Meditation Guide app.','Meditation Guide');
