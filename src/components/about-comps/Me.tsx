aed2be4239e8e4529c819cc8b2710dd16b2c1922
XCEVOR <XCEVOR@GMAIL.COM>
2023-10-12 17:50:07 +0900

    style: Apply margin top on second info div

--- src/components/about-comps/Me.tsx
+++ src/components/about-comps/Me.tsx
@@ -158,25 +158,25 @@ const Me = async () => {
               </div>
             </div>
 
-            <Reveal mydelay={2 * 1.8} >
-              {infoGroupBData 
-                ?
-                <div>
-                  {infoGroupBData.map((infob:aboutMeDataType, index:number) => (
-                    <p className='mt-4'>{infob.detail}</p>
-                  ))}
-                </div>
-                :
-                <p className='mt-4'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-              }
-
-
-            </Reveal>
+            <div className='mt-12'>
+              <Reveal mydelay={2 * 1.8} >
+                {infoGroupBData 
+                  ?
+                  <div>
+                    {infoGroupBData.map((infob:aboutMeDataType, index:number) => (
+                      <p className='mt-4'>{infob.detail}</p>
+                    ))}
+                  </div>
+                  :
+                  <p className='mt-4'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                }
+              </Reveal>
+            </div>
           </div>
 
         </div>
