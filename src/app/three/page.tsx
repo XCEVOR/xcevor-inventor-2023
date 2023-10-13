46e909fe7ad4708446f01577ac3404be1dbc7f79
XCEVOR <XCEVOR@GMAIL.COM>
2023-10-13 15:02:31 +0900

    feat: Create main text and bracket

--- src/app/three/page.tsx
+++ src/app/three/page.tsx
@@ -5,6 +5,8 @@ import { Canvas } from '@react-three/fiber';
 import { Environment, Scroll, ScrollControls, Sparkles } from '@react-three/drei';
 import BackgroundImage from '@/components/BackgroundImage';
 
+import { MainText } from '@/components/three-models/text/MainText';
+import { HeaderBracket } from '@/components/three-models/object/HeaderBracket';
 
 
 const Three = () => {
@@ -20,9 +22,18 @@ const Three = () => {
           <spotLight position={[0, 25, 0]} angle={1.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
           <Environment preset='studio' />
 
-          
+          {/* <Sparkles size={2} color={"#fff"} scale={[18,10,10]}></Sparkles> */}
+
           <ScrollControls pages={6} damping={0.1}>
             <Sparkles size={2} color={"#fff"} scale={[18,10,10]}></Sparkles>
+
+            <MainText position={[0, 0, 0.1]}/>
+            
+
+            <Scroll>
+              <HeaderBracket position={[2.4, 0, 0]} scale={1} />
+            </Scroll>
+            
           </ScrollControls>
         </Canvas>
       </div>
