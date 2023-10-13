acc2ca2adba983fe101380d9d9411c398aa64454
XCEVOR <XCEVOR@GMAIL.COM>
2023-10-13 13:01:06 +0900

    style: Adjust item size for responsive web

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -43,13 +43,13 @@ const Portfolio = async () => {
           ))}
         </div> */}
         
-        <div className='flex px-0 md:px-20 items-center justify-center  '>
-          <div className="grid lg:grid-cols-2 2xl:grid-cols-3 w-full max-w-fit">
+        <div className='flex px-1 md:px-20 items-center justify-center  '>
+          <div className="grid lg:grid-cols-2 2xl:grid-cols-3 w-full max-w-full">
             {featureProducts.map((item, index) => (
               <Reveal mydelay={1.5 * (1 + 0.4 * index)} >
               <div className='sm:p-8'>
 
-                <Link className="w-full h-[60vh] border-r-2 border-b-2 border-[#F8EFE4]/[2%]  flex flex-col justify-between group" href={`/portfolio/${item.id}`} key={item.id}> 
+                <Link className="w-full h-[90vw] md:max-h-[80vw] lg:max-h-[30vw] border-r-2 border-b-2 border-[#F8EFE4]/[2%]  flex flex-col justify-between group" href={`/portfolio/${item.id}`} key={item.id}> 
 
                   {/* IMAGE CONTAINER */}
                   {item.img && (
