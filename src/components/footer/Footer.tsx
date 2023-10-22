fb018a03afdda5227790f7dd99f839b4ba799bc1
XCEVOR <XCEVOR@GMAIL.COM>
2023-10-22 16:40:28 +0900

    style: Set the position of items

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,9 +4,14 @@ import Link from 'next/link';
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-[#f7f3ed] flex items-center justify-between   backdrop-blur-[0.5rem] bg-[#221e22] bg-opacity-75'>
-      <Link href="/" className='font-bold text-xl'>XXX</Link>
-      <p className=' text-lg'>Copyright © {currentYear} All rights reserved by Hochan</p>
+    <div className='flex flex-col items-center  h-fit px-0 md:px-20 lg:px-40 py-28   backdrop-blur-[0.5rem] bg-[#221e22] bg-opacity-75'>
+      <div className='h-12 relative flex flex-col  w-[90vw] lg:w-[80vw] items-center justify-center '>
+        <div className='flex items-center justify-between w-full'>
+          <Link href="/" className='font-bold text-xl'>XXX</Link>
+          <Link href="/" className='font-bold text-xl'>XXX</Link>
+        </div>
+        <p className='mt-8 text-lg'>Copyright © {currentYear} All rights reserved by Hochan</p>
+      </div>
     </div>
   )
 }
