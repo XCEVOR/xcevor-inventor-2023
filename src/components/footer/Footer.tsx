f6f7aa84b3b03e01f360490d4b6ad09ba850a733
XCEVOR <XCEVOR@GMAIL.COM>
2023-10-22 16:16:09 +0900

    style: Applay backdrop blur

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-[#FAD02C] flex items-center justify-between'>
+    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-[#f7f3ed] flex items-center justify-between   backdrop-blur-[0.5rem] bg-[#221e22] bg-opacity-75'>
       <Link href="/" className='font-bold text-xl'>XXX</Link>
       <p>@2023</p>
     </div>
