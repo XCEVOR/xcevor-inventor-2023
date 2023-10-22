b89bd282790ab09a3dc0abbc9f4d877a1b0105f2
XCEVOR <XCEVOR@GMAIL.COM>
2023-10-22 16:26:27 +0900

    feat: Create copyright text with currentYear

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-[#f7f3ed] flex items-center justify-between   backdrop-blur-[0.5rem] bg-[#221e22] bg-opacity-75'>
       <Link href="/" className='font-bold text-xl'>XXX</Link>
-      <p>@2023</p>
+      <p className=' text-lg'>Copyright © {currentYear} All rights reserved by Hochan</p>
     </div>
   )
 }
