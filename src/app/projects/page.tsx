bec05a09fc8158578f74a60456f3ad1786aa7e70
XCEVOR <XCEVOR@GMAIL.COM>
2023-10-23 22:22:43 +0900

    style: Modify backdrop color to bg-[#221e22]

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -65,11 +65,11 @@ const Portfolio = async () => {
                         // <Link className="w-fit h-[90vw] sm:max-h-[90vw] lg:max-h-[25vw] aspect-square   border-r-2 border-b-2 border-[#F8EFE4]/[2%]  flex flex-col justify-between group" href={`/projects/${item.id}`} key={item.id} >
                         <Link className="aspect-square   border-r-2 border-b-2 border-[#F8EFE4]/[2%]  flex flex-col justify-between group" href={`/projects/${item.id}`} key={item.id} >
                         {/* <div className="relative flex items-center justify-center h-[100%] aspect-square rounded-3xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border border-gray-200 border-opacity-10 transition-all duration-700 hover:scale-105"> */}
-                        <div className="relative flex items-center justify-center h-[100%] aspect-square rounded-3xl  bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border border-gray-200 border-opacity-10 transition-all duration-700 hover:scale-105">
+                        <div className="relative flex items-center justify-center h-[100%] aspect-square rounded-3xl   bg-[#221e22] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border border-gray-200 border-opacity-10 transition-all duration-700 hover:scale-105">
                           {/* <div className="relative h-[86%] aspect-square rounded-2xl overflow-hidden"> */}
                           <div className="relative h-[86%] aspect-square  ">
                             <Image src={`${process.env.DEV_FILESERVER_URL}${item.img}`} alt="" fill className='object-contain  '/>
-                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center bg-[#282120]/[90%] text-2xl text-[#F8EFE4] font-semibold">
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center bg-[#221e22]/[90%] text-2xl text-[#F8EFE4] font-semibold">
                               <div className='flex flex-col items-center justify-center'>
                                 <p>{item.title}</p>
                                 <p className='mt-2 text-[1rem]'>{item.catSlug.toUpperCase()}</p>
