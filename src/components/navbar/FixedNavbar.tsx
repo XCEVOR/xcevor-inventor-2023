4320351484e476f0dfc7dfd69cd853b121c19940
XCEVOR <XCEVOR@GMAIL.COM>
2023-10-23 22:36:03 +0900

    style: Adjust size and color of navbar

--- src/components/navbar/FixedNavbar.tsx
+++ src/components/navbar/FixedNavbar.tsx
@@ -48,7 +48,7 @@ export default function FixedNavbar() {
     <header className="z-[999] relative">
       <motion.div
         // className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-[#F8EFE4] border-opacity-40 bg-[#F8EFE4] bg-opacity-80 shadow-[4px_4px_4px_4px_rgba(0,0,0,0.3)] shadow-[#F8EFE4]/[0.1] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-[#0A0A0A] dark:border-[#0A0A0A]/40 dark:bg-opacity-75"
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-[#F8EFE4] border-opacity-40 bg-[#F8EFE4] bg-opacity-80  backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-[#0D0B0D] dark:border-[#040404]/40 dark:bg-opacity-75"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-[#F8EFE4] border-opacity-40 bg-[#F8EFE4] bg-opacity-80  backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.85rem] sm:w-[36rem] sm:rounded-full dark:bg-[#0D0B0D] dark:border-[#040404]/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
@@ -71,7 +71,7 @@ export default function FixedNavbar() {
                 //   }
                 // )}
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 x-10 text-[1rem] font-bold hover:text-gray-950 transition dark:text-[#F8EFE4] dark:hover:text-[#FAD02C]",
+                  "flex w-full items-center justify-center px-3 py-4 x-10 text-[1.1rem] font-bold hover:text-gray-950 transition dark:text-[#F7F3ED] dark:hover:text-[#edf048]",
                   {
                     "text-gray-950 dark:!text-[#282120]":
                       ( (link.hash === "/" && path === "/") || link.hash === "/" + path.split("/")[1] ),
@@ -87,7 +87,7 @@ export default function FixedNavbar() {
 
                 {( (link.hash === "/" && path === "/") || link.hash === "/" + path.split("/")[1] ) && (
                   <motion.span
-                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-[#FAD02C] "
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-[#edf048] "
                     // className="absolute inset-0 -x-10 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 rounded-full dark:border-[#FAD02C] dark:text-purple-400 dark:hover:text-white dark:hover:bg-[#FAD02C]/[50%] dark:focus:ring-purple-900 transition duration-300 ease-in-out"
                     // layoutId="activeSection"
                     // transition={{
