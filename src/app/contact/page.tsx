72b2911714691c13d88d386d4f8695d65e581117
XCEVOR <XCEVOR@GMAIL.COM>
2023-10-25 16:33:38 +0900

    style: Apply background, pagewrapper n whitespace

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,20 @@
+import BackgroundImage from '@/components/BackgroundImage'
+import { PageWrapper } from '@/components/framer-motion/page-wrapper'
 import React from 'react'
 
 const ContactMe = () => {
   return (
-    <div>ContactMe</div>
+    <>
+      <div className='flex flex-col items-center  h-fit px-0 lg:px-40 py-28 '>
+        <BackgroundImage />
+        {/* <div className='relative flex flex-col  lg:h-[calc(100vh-9.4rem)] w-[90vw] lg:max-w-[80rem] '> */}
+        <div className='relative flex flex-col  w-[90vw] lg:max-w-[80rem] '>
+          <PageWrapper>
+            <h2>ContactMe</h2>
+          </PageWrapper>
+        </div>
+      </div>
+    </>
   )
 }
 
