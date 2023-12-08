54c78e2ae8d9aad9aae59eb5c0c8bb960c4866d9
HOCHAN <XCEVOR@GMAIL.COM>
2023-12-08 23:57:17 +0900

    feat: Specify review output based on null

--- src/components/project-comps/ProductReview.tsx
+++ src/components/project-comps/ProductReview.tsx
@@ -29,21 +29,24 @@ const ProductReview = async ( {params}: {params: {id:string, titleSlug:string}} 
   const singleDescProduct:ProductDescType[] = await getDescData( params.titleSlug );
 
   return (
-    // <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-[#edf048] md:flex-row md:gap-8 md:items-center">
-    <div className='flex flex-col gap-6 lg:gap-12  '>
-      <div className='w-1/2 translate-x-1/2   border-t border-white/40'></div>
-      <h3 className='mt-12 mb-0 -lg:mb-6 text-3xl font-bold'>Project Review</h3>
+    <>
+    {singleDescProduct.map((desc, index) => (
+      desc.review !== null && (
+        <div className='flex flex-col gap-6 lg:gap-12  '>
+          <div className='w-1/2 translate-x-1/2   border-t border-white/40'></div>
+          <h3 className='mt-12 mb-0 -lg:mb-6 text-3xl font-bold'>Project Review</h3>
 
-      <div className="relative h-fit mt-4 mb-16 lg:mt-0 p-6  border border-white border-opacity-20  bg-[#221e22]/80 backdrop-blur rounded-xl">
-      {singleDescProduct.map((desc, index) => (
-          <div key={index} >{desc.review.split("|").map((rev, i) => (
-              <p>{rev}</p>
-          ))}</div>
-        ))
-      }
-      </div>
-
-    </div>
+          <div className="relative h-fit mt-4 mb-16 lg:mt-0 p-6 border border-white border-opacity-20 bg-[#221e22]/80 backdrop-blur rounded-xl">
+            <div key={index}>
+              {desc.review.split("|").map((rev, i) => (
+                <p key={i}>{rev}</p>
+              ))}
+            </div>
+          </div>
+        </div>
+      )
+    ))}
+    </>
   )
 }
 
