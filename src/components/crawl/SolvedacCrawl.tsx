2cf35526fbde8f4cc27adff03f3cab864c118595
XCEVOR <XCEVOR@GMAIL.COM>
2024-02-04 17:13:56 +0900

    feat: Create text component of crawled data

---
+++ src/components/crawl/SolvedacCrawl.tsx
@@ -0,0 +1,58 @@
+// src>app>component>crawl>SolvedacCrawl.tsx
+
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+interface CrawlResult {
+  rank?: string;
+  error?: string;
+}
+
+const SolvedacCrawl = () => {
+  const [crawlData, setCrawlData] = useState<CrawlResult>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/solvedac');
+        console.log("response:", response)
+        const data: CrawlResult = await response.json();
+        // setCrawlData(data);
+        
+        // Split the string based on the word '전체' and extract only the latter part
+        const rankParts = data.rank?.split('전체');
+        const rankWithoutPrefix = rankParts && rankParts[1]?.trim();
+
+        // Update status only when "rankWithoutPrefix" exists
+        if (rankWithoutPrefix) {
+          setCrawlData({ rank: rankWithoutPrefix });
+        } else {
+          console.error('This is not a valid data format.');
+          setCrawlData({ error: 'Data format error' });
+        }
+
+        console.log("data:", data)
+      } catch (error) {
+        console.error('There was a problem while crawling:', error);
+        setCrawlData({ error: 'Failed to load data.' });
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {crawlData.error ? (
+        <p>ERROR: {crawlData.error}</p>
+      ) : (
+        <div>
+          <p>Crawled Data: Rank {crawlData.rank}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SolvedacCrawl;
